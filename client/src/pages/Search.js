be1d4b9e5605d1e227fdff398526261a28af578b
Charith3426 <jk1856@srmist.edu.in>
2025-02-11 13:34:17 +0530

    code added

---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import RecipeList from '../components/search/RecipeList'
+import SearchForm from '../components/search/SearchForm'
+
+function Search() {
+    return (
+        <div>
+            <SearchForm />
+            <RecipeList />
+        </div>
+    )
+}
+
+export default Search
